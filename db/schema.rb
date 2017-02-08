d91d956be651e9c34cb5a91b55ce3a451972b62e
booox <booox@users.noreply.github.com>
2017-02-08 20:15:55 +0800

    implement admin products upload photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208112816) do
+ActiveRecord::Schema.define(version: 20170208120401) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170208112816) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
   create_table "users", force: :cascade do |t|
