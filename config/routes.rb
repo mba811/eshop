af5bae95abac7666326b039e6345377804ee32ad
booox <booox@users.noreply.github.com>
2017-02-08 19:26:50 +0800

    implement admin products CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
+  namespace :admin do
+    resources :products
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
